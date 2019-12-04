8b39e4b4edbe5255e127859cf6d25232359b7ad7
niuyi <niuyi@xiaomi.com>
2019-12-04 20:44:05 +0800

    add db test

---
+++ app/src/main/java/com/niuyi/androiddemo/DBHelper.java
@@ -0,0 +1,34 @@
+package com.niuyi.androiddemo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DB_NAME = "demo_db";
+
+    public static final String TABLE_NAME = "demo_data";
+    public static final String COLUMNS_NAME = "name";
+
+    private static final String SQL_TABLE_CREATE = "create table "
+            + TABLE_NAME + "("
+            + COLUMNS_NAME + " text"
+            + ");";
+
+    public DBHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/niuyi/androiddemo/InsertActivity.java
@@ -0,0 +1,101 @@
+package com.niuyi.androiddemo;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.niuyi.androiddemo.DBHelper.COLUMNS_NAME;
+import static com.niuyi.androiddemo.DBHelper.TABLE_NAME;
+
+public class InsertActivity extends AppCompatActivity {
+
+    private static final String TAG = InsertActivity.class.getCanonicalName() + "_LOG";
+    private SQLiteDatabase mDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+
+        ButterKnife.bind(this);
+
+        DBHelper mDBHelper = new DBHelper(this);
+        mDB = mDBHelper.getWritableDatabase();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick(R.id.btn_clean)
+    public void cleanDB(){
+        int count = mDB.delete(TABLE_NAME, null, null);
+        Toast.makeText(this, "delete done : " + count, Toast.LENGTH_LONG).show();
+    }
+
+    @OnClick(R.id.btn_insert)
+    public void insert(){
+
+        Log.i(TAG, "start insert3");
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+                long started = SystemClock.uptimeMillis();
+
+                mDB.beginTransaction();
+
+                try{
+                    ContentValues values = new ContentValues();
+
+                    for(int i = 0 ; i < 100000 ; i++){
+                        values.put(COLUMNS_NAME, "name: " + i);
+                        mDB.insert(TABLE_NAME, null, values);
+                        values.clear();
+                    }
+
+                    mDB.setTransactionSuccessful();
+                }finally {
+                    mDB.endTransaction();
+                }
+
+//                try{
+//
+//                    for(int i = 0 ; i < 100000 ; i++){
+//                        ContentValues values = new ContentValues();
+//                        values.put(COLUMNS_NAME, "name: " + i);
+//                        mDB.insert(TABLE_NAME, null, values);
+//                    }
+//
+//                    mDB.setTransactionSuccessful();
+//                }finally {
+//                    mDB.endTransaction();
+//                }
+
+                long timeInMethod = SystemClock.uptimeMillis() - started;
+
+                Log.i(TAG, "insert cost: " + timeInMethod);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(InsertActivity.this, "insert done" , Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }.start();
+
+
+    }
+}
